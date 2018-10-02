4aca8b1882858c985f9311c00355a7db1215a6e5
Aditya Kumar <aditya30394@gmail.com>
2018-10-02 03:40:56 +0000

    Final set of unit test changes

---
+++ rottenpotatoes/spec/module_modulehelper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# used this link https://stackoverflow.com/questions/1542945/testing-modules-in-rspec
+
+class DummyClass
+end
+
+describe 'MoviesHelper' do
+    
+    before(:each) do
+      @dummy_class = DummyClass.new
+      @dummy_class.extend(MoviesHelper)
+    end
+
+    it "check oddness function" do
+      expect(@dummy_class.oddness(2)).to eq "even"
+    end
+end
